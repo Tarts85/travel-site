4bbddbb06d76c3664f8ade2e97c506eeb261f3e6
Tarmo <tarmo.kiik@gmail.com>
2018-04-13 23:24:37 +0300

    Added mobile menu javascript and style.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,13 +1,3 @@
-function Person(fullName, favColour) {
-	this.name = fullName;
-	this.favouriteClolour = favColour;
-	this.greet = function() {
-		console.log('Hello, my name is ' + this.name + ' and my favourite colour is ' + this.favouriteClolour + '.');
-	}
-}
+import MobileMenu from './modules/MobileMenu';
 
-var john = new Person('John Doe', 'blue');
-john.greet();
-
-var jane = new Person('Jane Smith', 'green');
-jane.greet();
+var mobileMenu = new MobileMenu();
